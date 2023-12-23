0b8f8236e52fac9c38c224c0186a1f841f704d7a
smgorelik <smgorelik@gmail.com>
2023-12-23 01:00:17 -0500

    Create Client.cs
    Adding a client that can connect to Microsoft Azure, Microsoft EDR.
    In this example i am printing the pulled devices that are not bring your own devices

---
+++ MicrosoftGraph/Client.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Azure.Identity;
+using System.Configuration;
+using Microsoft.Graph.Models.ODataErrors;
+using Microsoft.Graph.Models;
+using System.Collections.Generic;
+
+namespace GraphClient
+{
+	class Program
+	{
+		public static async Task ListRegisteredDevices(GraphServiceClient graphClient)
+		{
+			var devices = new List<Device>();
+			try
+			{
+				var result = await graphClient.Devices.GetAsync((requestConfiguration) =>
+				{
+					/*https://learn.microsoft.com/en-us/graph/aad-advanced-queries?tabs=http#device-properties
+					  https://learn.microsoft.com/en-us/graph/api/resources/device?view=graph-rest-1.0*/
+
+					//Not including  - Workplace (indicates bring your own personal devices)
+					requestConfiguration.QueryParameters.Filter = "TrustType eq 'AzureAd' or TrustType eq 'ServerAd'";
+					requestConfiguration.QueryParameters.Select = new string[] { "id", "DisplayName", "OperatingSystem", "ProfileType", "TrustType" };
+				});
+				//Console.WriteLine(result);
+
+				var pageIterator = PageIterator<Device, DeviceCollectionResponse>.CreatePageIterator(graphClient,result,
+						(device) =>
+						{
+							devices.Add(device);						
+							return true;
+						});
+
+				await pageIterator.IterateAsync();
+
+				Console.WriteLine("Computers list number: " + devices.Count);
+				Console.WriteLine("{0,-15} {1,-10} {2,-10}", "DisplayName", "OperatingSystem", "Type");
+				foreach (Device dv in devices)
+				{
+					Console.WriteLine("{0,-20} {1,10} {2,-10}", dv.DisplayName, dv.OperatingSystem, dv.TrustType);
+				}
+				/*
+				 * You can do what ever you want with this devices now
+				 */
+				Console.WriteLine("-----------------------------------------");
+			}
+			catch (ODataError odataError)
+			{
+				Console.WriteLine(odataError.Error.Message);
+			}
+		}
+		public static async Task Main(string[] args)
+		{
+			string[] scopes = { "https://graph.microsoft.com/.default" };
+			var tenantId = ConfigurationManager.AppSettings["tenantId"];
+			var clientId = ConfigurationManager.AppSettings["clientId"];
+			var secret = ConfigurationManager.AppSettings["appSecret"];
+			var ClientSecretCredential = new ClientSecretCredential(
+				tenantId, clientId, secret);
+			var graphClient = new GraphServiceClient(ClientSecretCredential, scopes);
+			await ListRegisteredDevices(graphClient);
+		}
+	}
+}
